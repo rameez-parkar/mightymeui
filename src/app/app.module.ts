7317a8fa279e5110adcb8b69b7bdfb42c1e5b3cb
Rameez Parkar <rameez.parkar@gokloud.io>
2021-07-27 12:10:04 +0530

    created date-selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimeSelectorComponent } from './components/time-selector/time-selector.component';
+import { DateSelectorComponent } from './components/date-selector/date-selector.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimeSelectorComponent
+    TimeSelectorComponent,
+    DateSelectorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/date-selector/date-selector.component.ts
@@ -0,0 +1,69 @@
+import { Component, Input, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-date-selector',
+  templateUrl: './date-selector.component.html',
+  styleUrls: ['./date-selector.component.scss']
+})
+export class DateSelectorComponent implements OnInit {
+  @Input() selectedDate;
+  date = moment();
+  datesArr = [];
+  daysArr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.datesArr = this.createCalendar(this.date);
+  }
+
+  createCalendar(month) {
+    let firstDay = moment(month).startOf('M');
+    let days = Array.apply(null, {length: month.daysInMonth()})
+      .map(Number.call, Number)
+      .map(i => moment(firstDay).add(i,'d'));
+    for (let i=0; i < firstDay.weekday(); i++) {
+      days.unshift(null);
+    }
+    return days;
+  }
+
+  todayCheck(date) {
+    if (!date) {
+      return false;
+    }
+    return moment().format('L') === date.format('L');
+  }
+
+  nextMonth() {
+    this.date.add(1, 'M');
+    this.datesArr = this.createCalendar(this.date);
+  }
+
+  previousMonth() {
+    this.date.subtract(1, 'M');
+    this.datesArr = this.createCalendar(this.date);
+  }
+
+  onSelectDate(date) {
+    if (this.isDateSelectable(date)) {
+      let dateFormatted = date.format('MM/DD/YYYY');
+      this.selectedDate = dateFormatted;
+    }
+  }
+
+  isSelected(date) {
+    if (!date) {
+      return false;
+    }
+    let dateFormatted = moment(this.selectedDate, 'MM/DD/YYYY');
+    return dateFormatted.isSame(date);
+  }
+
+  isDateSelectable(selectedDate) {
+    if (moment(selectedDate).isBefore(moment())) {
+      return false;
+    }
+    return true;
+  }
+}
