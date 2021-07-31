767eb8614ec754724d0af54a38bf88427a7ec55d
Rameez Parkar <rameez.parkar@gokloud.io>
2021-07-31 18:04:09 +0530

    completed dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,41 @@ import { AppComponent } from './app.component';
 import { TimeSelectorComponent } from './components/time-selector/time-selector.component';
 import { DateSelectorComponent } from './components/date-selector/date-selector.component';
 import { SeatsAvailabilityComponent } from './components/seats-availability/seats-availability.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimeSelectorComponent,
     DateSelectorComponent,
-    SeatsAvailabilityComponent
+    SeatsAvailabilityComponent,
+    DashboardComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
